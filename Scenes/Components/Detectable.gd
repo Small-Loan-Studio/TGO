6fec86cef9d8a5c3fc7f77b7e05f8441adaa138d
envy <this-is-envy@users.noreply.github.com>
2024-07-11 21:22:10 -0700

    swap to signals

--- Scenes/Components/Detectable.gd
+++ Scenes/Components/Detectable.gd
@@ -1,20 +1,15 @@
 extends Area2D
 
-@export var _parent: Node2D = null
+signal detected(is_detected: bool)
 
 var _override_detection: bool = false
 
-func _ready() -> void:
-	if _parent == null:
-		_parent = get_parent()
-	_override_detection = _parent.has_method('set_detected')
-
 func set_detected(is_detected: bool) -> void:
-	if _override_detection:
-		_parent.set_detected(is_detected)
+	if detected.get_connections().size() > 0:
+		detected.emit(is_detected)
 	else:
 		# print('default set_detected(' + str(is_detected) + ')')
 		if is_detected:
-			_parent.show()
+			get_parent().show()
 		else:
-			_parent.hide()
+			get_parent().hide()
