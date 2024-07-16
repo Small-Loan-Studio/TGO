80c477f735f95456627f34e591fe920e2fea2c4b
envy <this-is-envy@users.noreply.github.com>
2024-07-16 12:59:03 -0700

    add lamp sensor visibility during editing

--- ZZ_Scratch/ScratchScenes.gd
+++ ZZ_Scratch/ScratchScenes.gd
@@ -2,7 +2,8 @@ class_name ScratchScenes
 extends Node
 
 const SCENES = {
-	"TestMovement": "res://Scratch/Movement/TestMovement.tscn",
+	"Test Movement & Graybox": "res://ZZ_Scratch/Movement/TestMovement.tscn",
+	"Peer Physics & Interactables": "res://ZZ_Scratch/PeerPhysics/PeerPhysics.tscn",
 }
 
 static func get_scenes() -> Dictionary:
