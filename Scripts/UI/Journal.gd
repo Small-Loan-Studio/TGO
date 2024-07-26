98e7cf5ec304dee3b95a3d103755b8e6a1c486ea
DenizDilek <denizdilek.bil@gmail.com>
2024-07-26 20:33:56 +0300

    Update Journal.gd
    working on quests

--- Scripts/UI/Journal.gd
+++ Scripts/UI/Journal.gd
@@ -2,3 +2,33 @@ extends Control
 
 class_name Journal
 
+#Dictionary to hold every ghost and monsters, will have a seen status to know if it should be shown to player.
+var bestiary : Dictionary = {}
+#Dictionary for quests
+#Quests are quite complicated so they will have their own object which will be stored in quests dictionary
+#Preferably a resource extension
+var quests : Array 
+#{
+	#1:{
+		#quest_name: "Capture 3 ghosts",
+		#description :"You have heard banshees are attacking villagers, banish them",
+		#requirements : {"Ghosts" : 3},
+		#reward : "Something"
+	#},
+#}
+func create_quest(id:int,quest_name:String,description:String,requirements:Dictionary,reward:String) -> Quest:
+		var quest  := Quest.new()
+		quest.id = id
+		quest.quest_name = quest_name
+		quest.description = description
+		quest.requirements = requirements
+		quest.reward = reward
+		return quest
+class Quest:
+	var id : int
+	var quest_name : String
+	var description : String
+	var requirements : Dictionary
+	var reward : String 
+	
+	
