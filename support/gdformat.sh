8a06e7b5adbdf405f8ab63bff20ad9030894206c
envy <this-is-envy@users.noreply.github.com>
2024-08-01 16:56:06 -0700

    docker tweaks

--- support/gdformat.sh
+++ support/gdformat.sh
@@ -4,6 +4,8 @@ args=$@
 
 if [ -z "$args" ]; then
   find . -iname *.gd | xargs ./venv/bin/gdformat --check
+elif [ "$args" == "--apply" ]; then
+  find . -iname *.gd | xargs ./venv/bin/gdformat
 else
   ./venv/bin/gdformat $args
 fi
