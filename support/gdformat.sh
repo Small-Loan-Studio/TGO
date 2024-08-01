9f07b00b7600ed11c41503e72084f1153b6ffdd1
envy <this-is-envy@users.noreply.github.com>
2024-08-01 16:59:36 -0700

    bash! eq check

--- support/gdformat.sh
+++ support/gdformat.sh
@@ -4,7 +4,7 @@ args=$@
 
 if [ -z "$args" ]; then
   find . -iname *.gd | xargs ./venv/bin/gdformat --check
-elif [ "$args" == "--apply" ]; then
+elif [ "$args" = "--apply" ]; then
   find . -iname *.gd | xargs ./venv/bin/gdformat
 else
   ./venv/bin/gdformat $args
