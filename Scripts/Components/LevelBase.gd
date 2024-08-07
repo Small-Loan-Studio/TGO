450fba8ef2611263636c3e8d82aab0369d7ba60a
this-is-envy <this-is-envy@users.noreply.github.com>
2024-08-07 12:18:47 -0700

    use the correct names; interfaces sure would be nice ^^

--- Scripts/Components/LevelBase.gd
+++ Scripts/Components/LevelBase.gd
@@ -5,7 +5,7 @@ var driver: Driver
 
 # @export var level_setup: Callable
 
-func _setup(driver_in: Driver) -> void:
+func setup(driver_in: Driver) -> void:
 	driver = driver_in
 	# if level_setup != null:
 	# 	level_setup.call()
