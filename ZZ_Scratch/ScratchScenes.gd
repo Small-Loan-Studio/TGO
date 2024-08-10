f6cf13a8eccd9613f4149732459db38d97d93998
envy <this-is-envy@users.noreply.github.com>
2024-08-10 11:42:58 -0700

    simple tileset + fix levelbase's default y-sort setting

--- ZZ_Scratch/ScratchScenes.gd
+++ ZZ_Scratch/ScratchScenes.gd
@@ -2,6 +2,7 @@ class_name ScratchScenes
 extends Node
 
 const SCENES = {
+	"Mario Demo": "res://ZZ_Scratch/MarioDemo/MarioDemo.tscn",
 	"Test Movement & Graybox": "res://ZZ_Scratch/Movement/TestMovement.tscn",
 	"Peer Physics & Interactables": "res://ZZ_Scratch/PeerPhysics/PeerPhysics.tscn",
 	"Audio Manager Volume Prefs": "res://ZZ_Scratch/AudioLevels/AudioLevels.tscn",
