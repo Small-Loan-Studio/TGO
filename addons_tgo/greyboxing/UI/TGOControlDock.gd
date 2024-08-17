6159bcb031238ff8b8013074ce8d37052f133c3c
envy <this-is-envy@users.noreply.github.com>
2024-08-17 02:15:53 -0700

    comments

--- addons_tgo/greyboxing/UI/TGOControlDock.gd
+++ addons_tgo/greyboxing/UI/TGOControlDock.gd
@@ -2,15 +2,21 @@
 class_name TGOControlDock
 extends Control
 
+## Used to identify the context in which we're operating on a level
 enum LevelSection {
-	UNKNOWN,
-	ROOT,
-	MAP,
-	OBJECTS,
-	MARKERS,
+	UNKNOWN,  # catch all / error state
+	ROOT,  #### base level data
+	MAP,  ##### the tilemap
+	OBJECTS,  # objects that exist in the level but aren't constrained to the grid
+	MARKERS,  # location markers within the level
 }
 
+## This is the scene loaded for editing objects
 const OBJECTS_HELPER_SCENE := "res://addons_tgo/greyboxing/UI/ObjectsHelper.tscn"
+const TILEMAP_SECTION_NAME := "TileMap"
+const OBJECTS_SECTION_NAME := "Objects"
+const MARKERS_SECTION_NAME := "Markers"
+
 
 var _plugin_ref: TGOGreyboxingToolsPlugin = null
 
@@ -18,19 +24,21 @@ var _selection: EditorSelection = null:
 	get:
 		return EditorInterface.get_selection()
 
+## Holds a reference to the interface that's been constructed and displayed to
+## edit a specific subsection. May be null if no UI is currently active
 var _subsection_control: Control = null
 
-var _active_section: Node = null:
-	get:
-		return _active_section
-	set(value):
-		print("_active_section: %s" % [value.name])
-		_active_section = value
+## The top-level node for a given section within the edited level, e.g.,
+## TileMap, Objects, or Marker.
+var _active_section: Node = null
+
+# UI references
 
 @onready var _selection_label: Label = %SelectedLabel
 @onready var _detail := %SectionDetails
 
 
+## called to establish any hooks to external references. Should be called post _ready
 func setup(plugin_ref: TGOGreyboxingToolsPlugin) -> void:
 	_plugin_ref = plugin_ref
 	var selection := EditorInterface.get_selection()
@@ -38,10 +46,12 @@ func setup(plugin_ref: TGOGreyboxingToolsPlugin) -> void:
 	_update_selection()
 
 
+## UI signal handler to trigger a plugin reload.
 func _reload() -> void:
 	_plugin_ref.reload()
 
 
+## Handler for the UI selection changing.
 func _update_selection() -> void:
 	var root := EditorInterface.get_edited_scene_root()
 	if !(root is LevelBase):
@@ -56,10 +66,17 @@ func _update_selection() -> void:
 		return
 
 	var section := _get_section(selected_node, root)
+	var next_section := _get_section_node(selected_node, root, section)
+	if _active_section == next_section:
+		# bail early so we can click around a section without trashing the UI
+		return
+
 	_active_section = _get_section_node(selected_node, root, section)
 	_update_section_control(section)
 
 
+## Returns the node currently selected in a scene. If no node, or multiple
+## nodes, are selected then it returns null.
 func _get_node(nodes: Array[Node]) -> Node:
 	if nodes.size() == 0:
 		_selection_label.text = "Editing: None"
@@ -71,7 +88,9 @@ func _get_node(nodes: Array[Node]) -> Node:
 	return nodes[0]
 
 
-func _get_section_node(selected: Node, root: Node, typ: LevelSection) -> Node:
+## Given a selected node, the edited Levelbase, and a target section return the
+## node corresponding to that section type. Returns null if things go awry.
+func _get_section_node(selected: Node, root: LevelBase, typ: LevelSection) -> Node:
 	if typ == LevelSection.UNKNOWN:
 		return null
 
@@ -79,35 +98,36 @@ func _get_section_node(selected: Node, root: Node, typ: LevelSection) -> Node:
 		return root
 
 	while selected != root:
-		if typ == LevelSection.MAP && selected.name == "TileMap":
+		if typ == LevelSection.MAP && selected.name == TILEMAP_SECTION_NAME:
 			return selected
-		if typ == LevelSection.OBJECTS && selected.name == "Objects":
+		if typ == LevelSection.OBJECTS && selected.name == OBJECTS_SECTION_NAME:
 			return selected
-		if typ == LevelSection.MARKERS && selected.name == "Markers":
+		if typ == LevelSection.MARKERS && selected.name == MARKERS_SECTION_NAME:
 			return selected
 
 		selected = selected.get_parent()
 
 	return null
 
-
+## Returns which section a selected node is in within a LevelBase.
 func _get_section(selected: Node, root: LevelBase) -> LevelSection:
 	if selected == root:
 		return LevelSection.ROOT
 
 	while selected != root:
 		match selected.name:
-			"TileMap":
+			TILEMAP_SECTION_NAME:
 				return LevelSection.MAP
-			"Objects":
+			OBJECTS_SECTION_NAME:
 				return LevelSection.OBJECTS
-			"Markers":
+			MARKERS_SECTION_NAME:
 				return LevelSection.MARKERS
 		selected = selected.get_parent()
 
 	return LevelSection.UNKNOWN
 
 
+## Updates state tracking
 func _update_section_control(section: LevelSection) -> void:
 	if _subsection_control != null:
 		_detail.remove_child(_subsection_control)
