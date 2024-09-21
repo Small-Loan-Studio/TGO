f889308d0ecee4af28102d0bd810936fce2ec031
Jdiprizio-msu <joseph_diprizio@yahoo.com>
2024-09-21 10:59:41 -0600

    Torch scene
    Simple torch scene with a pointlight, looking at making it interactable next, or maybe more customization options

---
+++ Scripts/Components/Greyboxing/Torch.gd
@@ -0,0 +1,16 @@
+@tool
+
+class_name Torch
+extends Node2D
+
+#I want to be able to interact with torch to turn on/off
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
