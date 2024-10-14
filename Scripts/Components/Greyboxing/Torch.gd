852f7029477eb2c455dffe7163bd17e11b78db28
Jdiprizio-msu <joseph_diprizio@yahoo.com>
2024-10-14 13:39:25 -0600

    First Pass of Torch Scene
    Added a torch scene

--- Scripts/Components/Greyboxing/Torch.gd
+++ Scripts/Components/Greyboxing/Torch.gd
@@ -3,14 +3,42 @@
 class_name Torch
 extends Node2D
 
-#I want to be able to interact with torch to turn on/off
+@export_category("Lighting")
+@export var light_color: Color = Color.WHITE:
+	get:
+		return light_color
+	set(value):
+		light_color = value
+		_change_light_color(light_color)
+@export var light_energy: float = 1.0:
+	get:
+		return light_energy
+	set(value):
+		light_energy = value
+		_change_light_energy(light_energy)
+@export var light_size: float = 0.75:
+	get:
+		return light_size
+	set(value):
+		light_size = value
+		_change_light_size(light_size)
+
+@onready var _pointLight2D: PointLight2D = $PointLight2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _toggle() -> void:
+	_pointLight2D.visible = !_pointLight2D.visible
+	print(_pointLight2D.visible)
 
+func _change_light_color(new_color: Color) -> void:
+	if _pointLight2D != null:
+		_pointLight2D.color = new_color
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _change_light_energy(new_energy: float) -> void:
+	if _pointLight2D != null:
+		_pointLight2D.energy = new_energy
+
+func _change_light_size(new_size: float) -> void:
+	if _pointLight2D != null:
+		_pointLight2D.texture_scale = new_size
+
