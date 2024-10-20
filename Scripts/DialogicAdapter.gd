d0cc51381e878dcc639e570f985161c7d5f694f0
this-is-envy <this-is-envy@users.noreply.github.com>
2024-10-20 09:24:43 -0700

    make inventory adapter generic to any inventory id

--- Scripts/DialogicAdapter.gd
+++ Scripts/DialogicAdapter.gd
@@ -1,8 +1,17 @@
 class_name DialogicAdapter
 extends Node
 
-class player_inventory:
-	static func has(item_name: String) -> bool:
+static var player_inventory := InventoryAdapter.new(Utils.PLAYER_ID)
+
+static func character_inventory(name: String) -> InventoryAdapter:
+	return InventoryAdapter.new(name)
+
+class InventoryAdapter:
+	var _id: String
+	func _init(id: String) -> void:
+		_id = id
+
+	func has(item_name: String) -> bool:
 		# TODO(envy): file issue that will validate item_name as a real item id
-		var inv := Driver.instance().inventory_mgr.get_inventory(Utils.PLAYER_ID)
+		var inv := Driver.instance().inventory_mgr.get_inventory(_id)
 		return inv.has_item_by_id(item_name)
