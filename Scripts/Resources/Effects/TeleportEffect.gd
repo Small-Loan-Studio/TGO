790cdd60f357f861ddb32aad5b2f202b02a2f91c
envy <166872119+this-is-envy@users.noreply.github.com>
2024-10-20 17:59:13 -0700

    [minor] Tweaks made during tutorial videos (#89)
    - introduce a DialogicAdapter and initial tooling within to check character inventory
    - Add tint options to greybox
    - bc teleport uses a nodepath that is relative at the time of construction do some futzing so that we can pull destinations from any height. basically un-nest Markers/<target>
    - add an extended simple tileset with three additional colors for passable tiles

--- Scripts/Resources/Effects/TeleportEffect.gd
+++ Scripts/Resources/Effects/TeleportEffect.gd
@@ -6,7 +6,18 @@ extends Effect
 
 
 func act(actor_id: String, cur_level: LevelBase) -> void:
-	var dest_node := parent.get_node(dest_path) as Node2D
+	var refined_path := str(dest_path)
+	var idx := refined_path.find(TGOControlDock.MARKERS_SECTION_NAME + "/")
+	if idx == -1:
+		printerr("Unable to find markers section in ", dest_path)
+		return
+
+	refined_path = refined_path.substr(idx)
+	var dest_node := cur_level.get_node(refined_path) as Node2D
+	if dest_node == null:
+		printerr("Unable to find teleport target ", refined_path)
+		return
+
 	var actor := cur_level.get_by_id(actor_id)
 	if actor != null:
 		actor.global_position = dest_node.global_position
