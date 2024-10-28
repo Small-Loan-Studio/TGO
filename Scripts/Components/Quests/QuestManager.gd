38c071f119f1eacd4309a3a330c7a47a9ca9c9a9
this-is-envy <this-is-envy@users.noreply.github.com>
2024-10-27 20:02:47 -0700

    quest registration

--- Scripts/Components/Quests/QuestManager.gd
+++ Scripts/Components/Quests/QuestManager.gd
@@ -1,7 +1,7 @@
 class_name QuestManager
 extends Node
 
-# Map<String, Quest>
+# Map<String, [Quest, path]>
 var _quest_dict: Dictionary = {}
 
 
@@ -11,6 +11,7 @@ func _ready() -> void:
 
 func _load_quests() -> void:
 	_load_quests_helper(Utils.QUEST_DIR)
+	print(_quest_dict)
 
 
 func _load_quests_helper(cur_dir: String) -> void:
@@ -27,10 +28,25 @@ func _load_quests_helper(cur_dir: String) -> void:
 		if dir.current_is_dir():
 			_load_quests_helper(abs_path)
 		else:
-			print("%s -> %s" % [path, abs_path])
 			var quest_res := load(abs_path) as Quest
-			print(quest_res)
+			if quest_res == null:
+				printerr("Non quest file found at ", abs_path)
+			else:
+				_register_quest(quest_res, abs_path)
 
 		path = dir.get_next()
 	
 	dir.list_dir_end()
+
+
+func _register_quest(q: Quest, path: String) -> void:
+	if q.id == "":
+		printerr("Likely misconfigured quest; no id specified: " + path)
+	if q.title == "":
+		printerr("Likely misconfigured quest; no title specified: %s / %s" % [q.id, path])
+
+	if _quest_dict.has(q.id):
+		var old_path: String = _quest_dict[q.id][1]
+		printerr("Duplicate quest IDs detected for '%s': %s & %s" % [q.id, old_path, path])
+		return
+	_quest_dict[q.id] = [q, path]
