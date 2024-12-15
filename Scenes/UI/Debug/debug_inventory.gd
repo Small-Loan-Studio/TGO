2ae3ce76ca3c4bf7eef5da089610a100f49dd8d4
Sunwolf <sunspotforest@gmail.com>
2024-12-15 14:48:15 -0600

    Appease linter and formatter

--- Scenes/UI/Debug/debug_inventory.gd
+++ Scenes/UI/Debug/debug_inventory.gd
@@ -1,23 +1,24 @@
 class_name DebugInventory
 extends HBoxContainer
 
-@onready var _inventory: Inventory
-@onready var ItemPicker : OptionButton = $ItemPicker
-@onready var QuantityPicker: OptionButton = $QuantityPicker
-@onready var item_dict: Dictionary = {}
-
-var itemStack : ItemStack
-var itemId: int
-var quantityId: int = -1
-var item: Item
-
 const ITEM_PATH = "res://Scripts/Resources/Items"
 
-func update_inv_options() ->void:
+var _item_stack: ItemStack
+var _item_id: int
+var _quantity_id: int = -1
+var _item: Item
+
+@onready var _inventory: Inventory
+@onready var _item_picker: OptionButton = $ItemPicker
+@onready var _quantity_picker: OptionButton = $QuantityPicker
+@onready var _item_dict: Dictionary = {}
+
+
+func update_inv_options() -> void:
 	#Loads configured items into the ItemPicker list
-	item_dict.clear()
-	ItemPicker.clear()
-	
+	_item_dict.clear()
+	_item_picker.clear()
+
 	var dir := DirAccess.open(ITEM_PATH)
 	if dir == null:
 		printerr("Failed to open item resource path:", DirAccess.get_open_error())
@@ -30,34 +31,39 @@ func update_inv_options() ->void:
 			var config := ResourceLoader.load(ITEM_PATH + "/" + item_file) as Item
 			if config != null:
 				var key := config.id
-				item_dict[key] = config
-				if ItemPicker:
-					ItemPicker.add_item(key)
+				_item_dict[key] = config
+				if _item_picker:
+					_item_picker.add_item(key)
 		item_file = dir.get_next()
+
 
 func setup(inv: Inventory) -> void:
 	_inventory = inv
 	update_inv_options()
 
+
 func remove_item() -> void:
-	itemId = ItemPicker.get_selected_id()
-	quantityId = QuantityPicker.get_selected_id() 
-	if (itemId == -1 or quantityId == -1):
+	_item_id = _item_picker.get_selected_id()
+	_quantity_id = _quantity_picker.get_selected_id()
+	if _item_id == -1 or _quantity_id == -1:
 		printerr("No item or quantity selected")
 		return
-	_inventory.remove_by_id(ItemPicker.get_item_text(itemId), int(QuantityPicker.get_item_text(quantityId)))
+	_inventory.remove_by_id(
+		_item_picker.get_item_text(_item_id), int(_quantity_picker.get_item_text(_quantity_id))
+	)
+
 
 func add_item() -> void:
-	itemId = ItemPicker.get_selected_id()
-	quantityId = QuantityPicker.get_selected_id()
-	if (itemId == -1 or quantityId == -1):
+	_item_id = _item_picker.get_selected_id()
+	_quantity_id = _quantity_picker.get_selected_id()
+	if _item_id == -1 or _quantity_id == -1:
 		printerr("No item or quantity selected")
 		return
-	item = item_dict[ItemPicker.get_item_text(itemId)]
-	if int(QuantityPicker.get_item_text(quantityId)) > item.stack_size:
+	_item = _item_dict[_item_picker.get_item_text(_item_id)]
+	if int(_quantity_picker.get_item_text(_quantity_id)) > _item.stack_size:
 		printerr("Trying to add more than allowable stack size")
 		return
-	itemStack = ItemStack.new()
-	itemStack.item = item
-	itemStack.quantity = int(QuantityPicker.get_item_text(quantityId))
-	_inventory.insert(itemStack)
+	_item_stack = ItemStack.new()
+	_item_stack.item = _item
+	_item_stack.quantity = int(_quantity_picker.get_item_text(_quantity_id))
+	_inventory.insert(_item_stack)
