6b38787373ba481fe1cb7c63306fb72696a81e88
this-is-envy <this-is-envy@users.noreply.github.com>
2024-12-25 16:30:07 -0800

    pr nits

--- Scenes/UI/Debug/DevinStateReport.gd
+++ Scenes/UI/Debug/DevinStateReport.gd
@@ -1,7 +1,6 @@
 extends Label
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	text = Driver.instance().player._state_machine.cur_state().name
 
