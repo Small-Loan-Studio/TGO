6e25b8bbafdf172a3f6ab8d91b88291158eb8efc
envy <this-is-envy@users.noreply.github.com>
2025-01-18 01:29:41 -0800

    fix layout load

--- addons_tgo/quest/quest_main_panel.gd
+++ addons_tgo/quest/quest_main_panel.gd
@@ -4,9 +4,10 @@ extends Control
 
 var _main_panel_ratio := .8
 var _editor: EditorInterface
+var _first_load := true
 
 @onready var _split_container := $VBoxContainer/HSplitContainer
-@onready var _graph_edit := $VBoxContainer/HSplitContainer/QuestGraphEdit
+@onready var _graph_edit: QuestGraphEdit = $VBoxContainer/HSplitContainer/QuestGraphEdit
 
 
 func _ready() -> void:
@@ -31,3 +32,10 @@ func _set_width() -> void:
 func _adjust_main_panel_ratio(offset: int) -> void:
 	var width := get_rect().size.x
 	_main_panel_ratio = (offset as float) / width
+
+
+func _on_visibility_changed() -> void:
+	if visible && _graph_edit != null && _first_load:
+		_first_load = false
+		await get_tree().create_timer(0.05).timeout
+		_graph_edit.do_layout()
