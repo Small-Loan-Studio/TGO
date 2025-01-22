2f695ff3b45701dcd572dc1861fbf2188582fbbe
Sunwolf <sunspotforest@gmail.com>
2025-01-22 16:31:35 -0600

    Linter pt 2

--- Scripts/DialogicAdapter.gd
+++ Scripts/DialogicAdapter.gd
@@ -17,20 +17,17 @@ class InventoryAdapter:
 	static var _item_dict: Dictionary = {}
 	var _id: String
 
-
 	func _init(id: String) -> void:
 		_id = id
 
-
 	static func _static_init() -> void:
-		var paths  := Utils.walk_directory(Item.ITEM_PATH, 
+		var paths  := Utils.walk_directory(Item.ITEM_PATH,
 		func(s: String) -> bool: return s.ends_with(".tres"))
 
 		for p in paths:
 			var item := ResourceLoader.load(Item.ITEM_PATH.path_join(p)) as Item
 			if item != null:
 				_item_dict[item.id] = item
-
 
 	func has(item_name: String, count: int = -1) -> bool:
 		# TODO(envy): file issue that will validate item_name as a real item id
