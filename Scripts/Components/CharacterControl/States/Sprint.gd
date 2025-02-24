5db663aa75edf7aa1ede8f94826232adcac9104b
envy <this-is-envy@users.noreply.github.com>
2025-02-23 18:15:28 -0800

    fast-follow on nits

--- Scripts/Components/CharacterControl/States/Sprint.gd
+++ Scripts/Components/CharacterControl/States/Sprint.gd
@@ -15,16 +15,14 @@ func enter(_state: Variant) -> void:
 		_state_machine.queue_state_change(idle_state)
 		return
 	_has_entered = true
-	run_input(null)
+	run_input(null, _state_machine.queue_state_change)
 
 
 func exit() -> void:
 	_has_entered = false
 
 
-func run_input(
-	_event: InputEvent, _change_state := func(state: State, ctx: Variant) -> void: pass
-) -> void:
+func run_input(_event: InputEvent, change_state: Callable) -> void:
 	_impulse = _ctx.controller.get_vector()
 
 	if _impulse != Vector2.ZERO:
@@ -36,15 +34,13 @@ func run_input(
 		if !animation_correct || !_animated_sprite.is_playing():
 			_animated_sprite.play(want_animation)
 	else:
-		_change_state.call(idle_state)
+		change_state.call(idle_state)
 
 	if !Enums.InputAction.SPRINT in _ctx.controller.get_button_pressed():
-		_change_state.call(walk_state, walk_state.mk_args(_impulse))
+		change_state.call(walk_state, walk_state.mk_args(_impulse))
 
 
-func run_physics(
-	_delta: float, _change_state := func(state: State, ctx: Variant) -> void: pass
-) -> void:
+func run_physics(_delta: float, _change_state: Callable) -> void:
 	if !_has_entered:
 		return
 	_ctx.character._sensor_group.rotation = _ctx.character.facing
@@ -56,11 +52,9 @@ func _get_stam() -> CharacterStat:
 	return _ctx.character.stats.get_stat(Enums.Stat.STAMINA)
 
 
-func run_tick(
-	delta: float, _change_state := func(state: State, ctx: Variant) -> void: pass
-) -> void:
+func run_tick(delta: float, change_state: Callable) -> void:
 	if _get_stam().value < stamina_drain_rate:
-		_change_state.call(idle_state)
+		change_state.call(idle_state)
 		return
 	_ctx.character.stats.get_stat(Enums.Stat.STAMINA).drain(stamina_drain_rate * delta)
 
