ecc50f9e6f79e7beed97b6769b92715fb43dd5f5
this-is-envy <this-is-envy@users.noreply.github.com>
2025-03-23 13:33:00 -0700

    exclude GeneratedSoundBanks, for now commit generated wwise ids, for now

---
+++ Scripts/Audio/wwise_ids.gd
@@ -0,0 +1,72 @@
+class_name AK
+
+class EVENTS:
+
+	const PLAYBACKGROUND = 1267621719
+	const WALKING = 340271938
+
+	const _dict = {
+		"PlayBackground": PLAYBACKGROUND,
+		"Walking": WALKING
+	}
+
+class STATES:
+
+	const _dict = {}
+
+class SWITCHES:
+
+	const _dict = {}
+
+class GAME_PARAMETERS:
+
+	const _dict = {}
+
+class TRIGGERS:
+
+	const _dict = {}
+
+class BANKS:
+
+	const INIT = 1355168291
+	const MAIN = 3161908922
+
+	const _dict = {
+		"Init": INIT,
+		"Main": MAIN
+	}
+
+class BUSSES:
+
+	const AMBIENT_SOUNDS = 3470839321
+	const MENU = 2607556080
+	const SOUND_EFFECTS = 978636652
+	const MAIN = 3161908922
+	const BACKGROUND_MUSIC = 4226739424
+
+	const _dict = {
+		"Ambient Sounds": AMBIENT_SOUNDS,
+		"Menu": MENU,
+		"Sound Effects": SOUND_EFFECTS,
+		"Main": MAIN,
+		"Background Music": BACKGROUND_MUSIC
+	}
+
+class AUX_BUSSES:
+
+	const _dict = {}
+
+class AUDIO_DEVICES:
+
+	const SYSTEM = 3859886410
+	const NO_OUTPUT = 2317455096
+
+	const _dict = {
+		"System": SYSTEM,
+		"No_Output": NO_OUTPUT
+	}
+
+class EXTERNAL_SOURCES:
+
+	const _dict = {}
+
