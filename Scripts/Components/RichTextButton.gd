a5b398d4fd0e8acafafb5200c17896e7c81eb49f
Sunwolf <sunspotforest@gmail.com>
2025-05-13 00:40:23 -0500

    Extended button added

---
+++ Scripts/Components/RichTextButton.gd
@@ -0,0 +1,37 @@
+class_name RichTextButton
+extends Button
+
+@onready var label: RichTextLabel = $RTL
+var openTags: String = ""
+var closeTags: String = ""
+var labelText: String = "Default"
+
+func _ready()->void:
+	update_button()
+
+func update_button()->void:
+	label.parse_bbcode("%s %s %s" % [openTags, labelText, closeTags])
+	custom_minimum_size = Vector2(label.get_content_width(),label.get_content_height())
+
+func set_rich_text(nText: String)->void:
+	labelText = nText
+
+func set_text_color(color: String)->void: # color is hexcode format XXXXXX
+	openTags = (openTags + "[color=#%s]" % [color])
+	closeTags = "[/color]" + closeTags
+
+func set_strikethrough()->void:
+	openTags = openTags + "[s]"
+	closeTags = "[/s]" + closeTags
+
+func clear_strikethrough()->void:
+	openTags = openTags.replace("[s]", "")
+	closeTags = closeTags.replace("[/s]","")
+
+func set_italics()->void:
+	openTags = openTags + "[i]"
+	closeTags = "[/i]" + closeTags
+
+func clear_italics()->void:
+	openTags = openTags.replace("[i]", "")
+	closeTags = closeTags.replace("[/i]", "")
