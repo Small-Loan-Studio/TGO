8bcac503677565bfb9a97502effa4307289376fe
Sunwolf <sunspotforest@gmail.com>
2025-06-08 02:23:36 -0500

    Visual clean up

--- Scripts/Components/RichTextButton.gd
+++ Scripts/Components/RichTextButton.gd
@@ -14,6 +14,7 @@ func update_button()->void:
 	label.parse_bbcode("%s %s %s" % [openTags, labelText, closeTags])
 	#custom_minimum_size = Vector2(label.get_content_width(),label.get_content_height())
 
+
 func set_button_size(width: float, length: float) -> void:
 	custom_minimum_size = Vector2(width,length)
 
@@ -29,19 +30,52 @@ func set_text_color(color: String)->void: # color is hexcode format XXXXXX
 	
 	openTags = (openTags + "[color=#%s]" % [color])
 	closeTags = "[/color]" + closeTags
+	
+	update_button()
+
+func remove_text_color()->void:
+	var idx: int = openTags.find("[color")
+	if idx >= 0:
+		openTags = openTags.erase(idx, 15)
+	closeTags = closeTags.replace("[/color]", "")
+	
+	update_button()
 
 func set_strikethrough()->void:
 	openTags = openTags + "[s]"
 	closeTags = "[/s]" + closeTags
+	
+	update_button()
 
 func clear_strikethrough()->void:
 	openTags = openTags.replace("[s]", "")
 	closeTags = closeTags.replace("[/s]","")
+	update_button()
 
 func set_italics()->void:
 	openTags = openTags + "[i]"
 	closeTags = "[/i]" + closeTags
+	update_button()
 
 func clear_italics()->void:
 	openTags = openTags.replace("[i]", "")
 	closeTags = closeTags.replace("[/i]", "")
+	update_button()
+
+func set_bold()->void:
+	openTags = openTags + "[b]"
+	closeTags = "[/b]" + closeTags
+	update_button()
+
+func clear_bold()->void:
+	openTags = openTags.replace("[b]", "")
+	closeTags = closeTags.replace("[/b]", "")
+	update_button()
+
+func focus_color()->void:
+	set_bold()
+	set_text_color("FFFFFF")
+
+func exit_focus_color()->void:
+	clear_bold()
+	set_text_color("BBBBBB")
--- addons/dialogic/Modules/TGOLayoutStyle/CenteredChoices/RichChoiceNode.gd
+++ addons/dialogic/Modules/TGOLayoutStyle/CenteredChoices/RichChoiceNode.gd
@@ -1,11 +1,11 @@
 extends DialogicNode_ChoiceButton
 
+
 ## Called when the text changes.
 func _set_text_changed(new_text: String) -> void:
 	for entry in Dialogic.History.get_simple_history():
 		if entry['event_type'] == 'Choice':
 			if entry['text'] == new_text:
 				text_node.set_strikethrough()
-				text_node.set_text_color("00FF00")
 	text_node.set_rich_text(new_text)
 	text_node.set_button_size(get_size().x, get_size().y)
