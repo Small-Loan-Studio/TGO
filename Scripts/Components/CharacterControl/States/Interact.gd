9ece0898a3b31c201128bbef415dea941339f490
this-is-envy <this-is-envy@users.noreply.github.com>
2025-06-25 03:19:20 -0700

    last tweak

--- Scripts/Components/CharacterControl/States/Interact.gd
+++ Scripts/Components/CharacterControl/States/Interact.gd
@@ -39,6 +39,7 @@ func run_input(_event: InputEvent, change_state: Callable) -> void:
 				_is_interacting = true
 				_is_selecting = false
 				interactable.secondary.toggle()
+				interactable.primary.visible = true
 				interactable.trigger(_ctx.character, interactable.secondary.selected)
 				await interactable.triggered
 		elif _ctx.controller.just_pressed(Enums.InputAction.DOWN):
