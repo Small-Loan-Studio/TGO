039dabf83c912660ee341b5f320c04ede9af2355
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-08-07 03:11:36 -0500

    Prevents walking/sprinting during dialog (#230)
    Co-authored-by: Sunwolf <sunspotforest@gmail.com>

---
+++ Scripts/Components/CharacterControl/States/Dialogue.gd
@@ -0,0 +1,15 @@
+extends CharacterState
+
+@export var idle_state: State
+
+
+func enter(_ctx: Variant) -> StateChange:
+	if Dialogic.current_timeline != null:
+		return StateChange.mk(idle_state)
+	return null
+
+
+func run_tick(_delta: float) -> StateChange:
+	if Dialogic.current_timeline != null:
+		return StateChange.mk(idle_state)
+	return null
